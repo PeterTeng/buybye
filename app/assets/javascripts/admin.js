4c8b6a2d754214b2e77e80890549db1ea546dd42
Yohei Senju <yohei.senju@gmail.com>
2016-10-03 04:00:48 +0900

    Add js css to vendor/assets

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,12 @@
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require metisMenu/jquery.metisMenu.js
+//= require pace/pace.min.js
+//= require peity/jquery.peity.min.js
+//= require slimscroll/jquery.slimscroll.min.js
+//= require iCheck/icheck.min.js
+//= require flash
+//= require inspinia.js
+//= require jquery-ui/sortable
+//= require table_sort
