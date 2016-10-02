90c3e40e9233354d679a5ffd840e628666adc291
Yohei Senju <yohei.senju@gmail.com>
2016-10-03 01:14:53 +0900

    Add needed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+  resources :users do
+    resources :exhibits
+    member do
+      get :dashboard
+    end
+  end
+  resources :comments
+  resources :messages
+  resources :chatrooms
+
+  get '/admin', to: 'admin/dashboard#index'
+  namespace :admin do
+    resources :users
+  end
 end
