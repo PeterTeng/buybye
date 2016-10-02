558b5f2b9f34971ab3d4c171ed6e931ea858d015
Yohei Senju <yohei.senju@gmail.com>
2016-10-03 02:21:18 +0900

    Implement users sign up func

---
+++ app/controllers/users_cotroller.rb
@@ -0,0 +1,58 @@
+class UsersController < ApplicationController
+
+  before_action :set_user, only: [:show, :edit, :update, :become_guide, :dashboard]
+  before_action :correct_user, only: [:edit, :update, :dashboard]
+
+  def dashboard
+  end
+
+  def new
+    if logged_in?
+      redirect_to user_path current_user
+    else
+      @user = User.new
+    end
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.used_email? @user.email
+      redirect_to new_user_path(flash[:danger] = 'this email address is already used')
+    else
+      if @user.save
+        log_in @user
+        remember @user
+        # SignUpNotifer.send_user(@user).deliver_now
+        redirect_to dashboard_user_path @user
+      else
+        render :new
+      end
+    end
+  end
+
+  private def set_user
+    @user = User.find params[:id]
+  end
+
+  private def user_params
+    params.
+      require(:user).
+        permit(
+          :email,
+          :password_digest,
+          :remember_digest,
+          :password,
+          :introduce,
+          :gender,
+          :undergraduate,
+          :department,
+          { :weekday [] },
+        )
+  end
+
+  private def correct_user
+    @user = User.find params[:id]
+    redirect_to dashboard_user_path @user unless correct_user? @user
+  end
+
+end
