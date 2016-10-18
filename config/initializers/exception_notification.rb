c74a416e6a7608317045892d8f6ba3df6ade5513
Yohei Senju <yohei.senju@gmail.com>
2016-10-18 19:54:56 +0900

    Implement exception notification for production

---
+++ config/initializers/exception_notification.rb
@@ -0,0 +1,14 @@
+require 'exception_notification/rails'
+
+ExceptionNotification.configure do |config|
+  config.ignore_if do |exception, options|
+    not Rails.env.production?
+  end
+
+  config.add_notifier :slack, {
+    webhook_url: Rails.application.secrets.slack_webhook_url,
+  }
+  config.ignored_exceptions = ''
+end
+
+Rails.application.config.middleware.use ExceptionNotification::Rack, ignore_crawlers: %w{Googlebot bingbot}
