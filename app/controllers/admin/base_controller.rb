fb0e2496bc1a4c69f0de153ac0582471225b65e4
Yohei Senju <yohei.senju@gmail.com>
2016-10-19 04:46:26 +0900

    Fix typo

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -5,7 +5,7 @@ class Admin::BaseController < ApplicationController
 
   layout "admin"
   private def authenticated_admin!
-    redirect_to :root_path unless current_user.try(:admin?)
+    redirect_to root_path unless current_user.try(:admin?)
   end
 
 end
