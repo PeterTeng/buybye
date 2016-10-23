338fc63a34079642c0eb6c90a2cdab9a8aa2fbce
Yohei Senju <yohei.senju@gmail.com>
2016-10-24 02:14:39 +0900

    Add user delete func

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,12 +2,16 @@ class Admin::UsersController < Admin::BaseController
 
   before_action :set_user, only: [:show, :destroy]
 
+  def index
+    @users = User.all
+  end
+
   def show
   end
 
   def destroy
     @user.destroy
-    redirect_to
+    redirect_to :back
   end
 
   private def set_user
