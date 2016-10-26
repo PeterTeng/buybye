2862137ade7c6016900c9bd9fe5db30383747d06
Yohei Senju <yohei.senju@gmail.com>
2016-10-26 12:34:02 +0900

    Add new transaction status

--- app/models/item.rb
+++ app/models/item.rb
@@ -25,7 +25,7 @@ class Item < ApplicationRecord
   belongs_to :undergraduate
   belongs_to :user
 
-  AVAILABLE_TRANSACTION_STATUS_HASH  = { "取引中" => 0, "取引完了" => 1 }
+  AVAILABLE_TRANSACTION_STATUS_HASH  = { "出品中" => 0, "取引中" => 1, "取引完了" => 2 }
   AVAILABLE_PRESERVATION_STATUS_HASH = { "新品・未使用" => 0, "未使用に近い" => 1, "目立った傷や汚れなし" => 2, "やや傷や汚れあり" => 3, "傷や汚れあり" => 4, "全体的に状態が悪い" => 5 }
   PRICE_LIMITATION                   = (10...100000)
 
