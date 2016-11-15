da0fd9527e50c3a18855f9b9f178449aac511fba
Yohei Senju <yohei.senju@gmail.com>
2016-11-15 16:39:25 +0900

    Remove useless script

--- config/initializers/recaptcha.rb
+++
@@ -1,4 +0,0 @@
-Recaptcha.configure do |config|
-  config.public_key  = Rails.application.secrets.google_recaptcha_site_key
-  config.private_key = Rails.application.secrets.google_recaptcha_secret_key
-end
