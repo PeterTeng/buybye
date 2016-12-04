5f522b375f3bcbf532a5d2436e9e36ffedd72d5e
Yohei Senju <yohei.senju@gmail.com>
2016-12-04 16:54:29 +0900

    Add detail information when registrate stripe

--- app/service/create_card_information_service.rb
+++ app/service/create_card_information_service.rb
@@ -8,6 +8,7 @@ class CreateCardInformationService
       last_4_number: card_information["last4"]
     }
     user.create_card_information card_information_params
-    NotifySlackWorker.new.perform("[Stripe] new user", ":moneybag:", "#{user.email}")
+    text = "name: #{card_information_params[:card_owner]}, ブランド名: #{card_information_params[:brand_name]}, 国: #{card_information_params[:country]}, 最後の4桁: #{card_information_params[:last_4_number]}"
+    NotifySlackWorker.new.perform("[Stripe] new user", ":moneybag:", text)
   end
 end
