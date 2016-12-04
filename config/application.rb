ce45922deb8f0458833c75dd45b8bee1f565a622
Yohei Senju <yohei.senju@gmail.com>
2016-12-04 20:11:08 +0900

    [WIP] Internationalize error messages

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Buybye
   class Application < Rails::Application
     config.autoload_paths += Dir["#{config.root}/lib"]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
