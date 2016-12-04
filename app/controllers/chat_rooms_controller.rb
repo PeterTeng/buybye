4207fd354f94b1fffe4a6aca4cccb000a0da8ed1
Yohei Senju <yohei.senju@gmail.com>
2016-12-05 00:40:08 +0900

    Detect N+1 query, Optimize it

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -5,17 +5,18 @@ class ChatRoomsController < ApplicationController
   layout "chat"
 
   def show
-    @chat_room = ChatRoom.includes(users: [:messages]).includes(:messages).find params[:id]
-    @item      = Item.includes(:item_images).includes(:user).includes(:buyer).find @chat_room.item_id
+    @chat_room = ChatRoom.find(params[:id])
+    @item      = Item.includes(:user).includes(:buyer).find @chat_room.item_id
     @exhibitor = @item.user
     @buyer     = @item.buyer
     @message   = Message.new
-    @messages  = @chat_room.messages.order created_at: :desc
+    @messages  = @chat_room.messages.includes(:user).order created_at: :desc
   end
 
 
   private def private_chat_room_except_admin!
-    unless current_user.admin || ChatRoom.find(params[:id]).users.include?(current_user)
+    user_ids = ChatRoom.find(params[:id]).users.pluck(:user_id)
+    unless current_user.admin || user_ids.include?(current_user.id)
       not_found
     end
   end
