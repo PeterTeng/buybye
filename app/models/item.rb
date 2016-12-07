c5e50614f04bfa608a597653d430d0fe8258f85b
Yohei Senju <yohei.senju@gmail.com>
2016-12-07 15:25:15 +0900

    Implement favorite func model

--- app/models/item.rb
+++ app/models/item.rb
@@ -29,6 +29,8 @@ class Item < ApplicationRecord
   has_many :like_users, foreign_key: :like_id
   has_many :liker, class_name: "User", through: :like_users
 
+  has_one :trade
+
   belongs_to :college
   belongs_to :depertment
   belongs_to :undergraduate
@@ -36,14 +38,23 @@ class Item < ApplicationRecord
   belongs_to :buyer, class_name: "User", foreign_key: :buyer_id
   belongs_to :chat_room
 
+  # before_save :change_price_rate!
+
+  mount_uploader :image, ImageUploader
+
   AVAILABLE_TRANSACTION_STATUS_HASH  = { "出品中" => 0, "取引中" => 1, "取引完了" => 2 }
   AVAILABLE_PRESERVATION_STATUS_HASH = { "新品・未使用" => 0, "未使用に近い" => 1, "目立った傷や汚れなし" => 2, "やや傷や汚れあり" => 3, "傷や汚れあり" => 4, "全体的に状態が悪い" => 5 }
   PRICE_LIMITATION                   = (10...100000)
 
   validates :name, presence: true, length: { maximum: 50 }
-  validates :price, presence: true, inclusion: { in: Item::PRICE_LIMITATION }
-
+  # validates :row_price, presence: true
+  validates :description, presence: true
   enum transaction_status: Item::AVAILABLE_TRANSACTION_STATUS_HASH
   enum preservation_status: Item::AVAILABLE_PRESERVATION_STATUS_HASH
 
+  private def change_price_rate!
+    self.update(
+      price_with_margin: self.row_price*1.1
+    )
+  end
 end
--- app/models/like_user.rb
+++ app/models/like_user.rb
@@ -12,10 +12,4 @@ class LikeUser < ApplicationRecord
   belongs_to :liker, class_name: "User"
   belongs_to :like, class_name: "Item"
 
-  def favorite!(user_id, item_id)
-    self.create(
-      liker_id: user_id,
-      like_id: item_id
-    )
-  end
 end
